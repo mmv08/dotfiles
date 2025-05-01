06b8de86756e8d00455575043c8653cb601c1cc8
Mikhail Mikheev <16622558+mmv08@users.noreply.github.com>
2025-05-01 12:16:22 +0200

    update zsh script

--- .dotfiles-meta/install_scripts/install_zsh.sh
+++ .dotfiles-meta/install_scripts/install_zsh.sh
@@ -14,15 +14,7 @@ if [ -d "${HOME}/.oh-my-zsh" ]; then
   echo "→ Oh My Zsh already installed, skipping installation"
 else
   echo "→ Installing Oh My Zsh"
-  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  export KEEP_ZSHRC=yes
+  export RUNZSH=no
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
-
-# Change the default shell to Zsh using a shell builtin
-ZSH_PATH=$(command -v zsh || true)
-if [[ -n "$ZSH_PATH" ]]; then
-  chsh -s "$ZSH_PATH" || echo "Warning: failed to change shell to $ZSH_PATH"
-else
-  echo "Warning: zsh not found in PATH; skipping default shell change."
-fi
-
-echo "Installation complete. Please log out and log back in to start using Zsh with Oh My Zsh."
